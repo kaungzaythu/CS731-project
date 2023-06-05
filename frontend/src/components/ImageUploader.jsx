92959fd71be8d2ce74fc7f89b21dc3d52a8933a2
Kaung Zay Thu <kaungzaythu@hotmail.com>
2023-06-05 08:26:25 -0600

    feat: integrate Material UI to Registration Page

--- frontend/src/components/ImageUploader.jsx
+++ frontend/src/components/ImageUploader.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const ImageUploader = ({onImageUpload}) => {
+const ImageUploader = ({ onImageUpload }) => {
   const [imageData, setImageData] = useState(null);
 
   const handleDrop = (event) => {
@@ -11,11 +11,8 @@ const ImageUploader = ({onImageUpload}) => {
     reader.onload = () => {
       const base64 = reader.result;
       setImageData(base64);
-      // Send base64 code to the server using an API call
-      // Example: sendToServer(base64);
-
       onImageUpload(base64);
-      console.log(`base64 is here => ${base64}`)
+      console.log(`base64 is here => ${base64}`);
     };
 
     reader.readAsDataURL(file);
@@ -30,21 +27,22 @@ const ImageUploader = ({onImageUpload}) => {
       onDrop={handleDrop}
       onDragOver={handleDragOver}
       style={{
-        width: '300px',
-        height: '300px',
-        border: '2px dashed #ccc',
-        borderRadius: '5px',
+        width: '150px',
+        height: '150px',
+        border: '2px dashed #7A3385',
+        borderRadius: '50%', // Make it a circle by setting borderRadius to 50%
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         cursor: 'pointer',
+        overflow: 'hidden', // Hide the parts of the image outside the circle
       }}
     >
       {imageData ? (
         <img
           src={imageData}
           alt="Uploaded Image"
-          style={{ maxWidth: '100%', maxHeight: '100%' }}
+          style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }} // Maintain aspect ratio and cover the circle area
         />
       ) : (
         <span>Drag and drop an image here</span>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { FaUser } from 'react-icons/fa'
 import { register, reset } from '../features/auth/authSlice'
 import Spinner from '../components/Spinner'
 import ImageUploader from '../components/ImageUploader';
-
+import Grid from '@mui/material/Grid';
+import CustomTextbox from '../components/CustomTextBox';
+import Typography from '@mui/material/Typography';
+import GradientButton from '../components/GradientButton'
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -79,102 +82,204 @@ function Register() {
 
   return (
     <>
-      <section>
-        <h1>
-          <FaUser /> Register
-        </h1>
-        <p>Please create an account</p>
-      </section>
+      
 
       <section>
         <form onSubmit={onSubmit}>
-          <div>
-            <input
-              type='text'
-              id='first_name'
-              name='first_name'
-              value={first_name}
-              placeholder='Fill in your first name'
-              onChange={onChange}
-            />
-          </div>
-          <div>
-            <input
-              type='text'
-              id='last_name'
-              name='last_name'
-              value={last_name}
-              placeholder='Fill in your last name'
-              onChange={onChange}
-            />
-          </div>
-          <div>
-            <input
-              type='text'
-              id='introduction'
-              name='introduction'
-              value={introduction}
-              placeholder='Fill in introdction. E.g. A passionate chef'
-              onChange={onChange}
-            />
-          </div>
-          <div>
-            <input
-              type='email'
-              id='email'
-              name='email'
-              value={email}
-              placeholder='Fill in your email'
-              onChange={onChange}
-            />
-          </div>
-          <div>
-            <input
-              type='text'
-              id='phone_number'
-              name='phone_number'
-              value={phone_number}
-              placeholder='Fill in your phone number'
-              onChange={onChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="profile_picture">Profile Picture</label>
-            <ImageUploader
-              onImageUpload={(base64) =>
+              <Grid container spacing={2} justifyContent="center">
+                  <Grid item xs={6}>
+
+                    <Grid item xs={12} >
+                      <Typography sx={{fontFamily:'Libre Caslon Text', paddingTop: '150px', paddingLeft: '160px'}} variant="h2" align="left">
+                        
+
+                      <span style={{ color: '#7A3385' }}>Friend</span><span style={{ color: '#335985' }}>Loop</span>
+
+                      </Typography>
+                      <Typography sx={{fontFamily:'Libre Caslon Text', paddingTop: '25px',  paddingLeft: '160px' }} variant="h6" align="left">
+                        
+
+                        <span style={{ color: '#335985' }}>Express Yourself, Connect with Others.</span>
+        
+                      </Typography>
+                    </Grid>
+
+                  </Grid>
+                  <Grid item xs={6} > 
+                      <Grid container spacing={2}  sx={{paddingTop: '50px'}}>
+                          {/* Initial Row */}
+                          <Grid item xs={4}>
+                          
+                          </Grid>
+                          <Grid item xs={4} align='center' >
+                            <ImageUploader
+                            onImageUpload={(base64) =>
+                              
+                              setFormData((prevState) => ({
+                                ...prevState,
+                                profile_picture: base64,
+                              }))
+                            }
+                            />
+                          </Grid>
+                          <Grid item xs={4}>
+                            
+                          </Grid>
+                          {/* First Row */}
+                          <Grid item xs={2}>
+                            
+                          </Grid>
+                          <Grid item xs={4}>
+                            <CustomTextbox
+                                type='text'
+                                id='first_name'
+                                name='first_name'
+                                value={first_name}
+                                placeholder='Fill in your first name'
+                                label="First Name"
+                                onChange={onChange}
+                              />
+                            
+                          </Grid>
+                          <Grid item xs={4}>
+                            <CustomTextbox
+                              type='text'
+                              id='last_name'
+                              name='last_name'
+                              value={last_name}
+                              placeholder='Fill in your last name'
+                              label="Last Name"
+                              onChange={onChange}
+                            />
+                            
+                          </Grid>
+                          <Grid item xs={2}>
+                            
+                          </Grid>
+                          {/* Second Row */}
+                            <Grid item xs={2}>
+                            
+                            </Grid>
+                            <Grid item xs={8}>
+                              <CustomTextbox
+                                type='text'
+                                id='introduction'
+                                name='introduction'
+                                value={introduction}
+                                placeholder='Fill in introdction. E.g. A passionate chef'
+                                label='Introduction'
+                                onChange={onChange}
+                              />
+                            </Grid>
+                            <Grid item xs={2}>
+                              
+                            </Grid>
+                             {/* Third Row */}
+
+                            <Grid item xs={2}>
+                              
+                              </Grid>
+                              <Grid item xs={8}>
+                                <CustomTextbox
+                                  type='text'
+                                  id='phone_number'
+                                  name='phone_number'
+                                  value={phone_number}
+                                  placeholder='Fill in your phone number'
+                                  label='Phone Number'
+                                  onChange={onChange}
+                                />
+                              </Grid>
+                              <Grid item xs={2}>
+                                
+                              </Grid>
+                              {/* Forth Row */}
+
+                              <Grid item xs={2}>
+                                
+                                </Grid>
+                                <Grid item xs={8}>
+                                  <CustomTextbox
+                                    type='email'
+                                    id='email'
+                                    name='email'
+                                    value={email}
+                                    placeholder='Fill in your email'
+                                    label='Email'
+                                    onChange={onChange}
+                                  />
+                                </Grid>
+                                <Grid item xs={2}>
+                                  
+                                </Grid>
+                                {/* Fifth Row */}
+                                <Grid item xs={2}>
+                              
+                              </Grid>
+                              <Grid item xs={4}>
+                                <CustomTextbox
+                                  type='password'
+                                  id='password'
+                                  name='password'
+                                  value={password}
+                                  placeholder='Enter password'
+                                  label='Password'
+                                  onChange={onChange}
+                                />
+                                
+                              </Grid>
+                              <Grid item xs={4}>
+                                <CustomTextbox
+                                  type='password'
+                                  id='password2'
+                                  name='password2'
+                                  value={password2}
+                                  label='Confirm Password'
+                                  placeholder='Confirm password'
+                                  onChange={onChange}
+                                />
+                                
+                              </Grid>
+                              <Grid item xs={2}>
+                              
+                              </Grid>
+                               {/* Sixth Row */}
+                               <Grid item xs={2}>
+                              
+                              </Grid>
+                               <Grid item xs={4} align="left"  sx={{fontFamily:'lato', color: '#335985'}}>
+                                Have an account? &nbsp;
+                              <Link to='/login'>
+                                    Login here
+                                </Link>
+                              </Grid>
+                              <Grid item xs={4} align="right">
+                              <GradientButton type="submit" text="REGISTER" />
+                              </Grid>
+                              <Grid item xs={2}>
+                              
+                              </Grid>
+
+                              {/* Sixth Row */}
+                      </Grid>
+                  </Grid>
+              </Grid>
+        
+              <Grid container spacing={2} sx={{paddingTop: '50px'}}>
+              
                 
-                setFormData((prevState) => ({
-                  ...prevState,
-                  profile_picture: base64,
-                }))
-              }
-            />
-          </div>
-          <div>
-            <input
-              type='password'
-              id='password'
-              name='password'
-              value={password}
-              placeholder='Enter password'
-              onChange={onChange}
-            />
-          </div>
-          <div>
-            <input
-              type='password'
-              id='password2'
-              name='password2'
-              value={password2}
-              placeholder='Confirm password'
-              onChange={onChange}
-            />
-          </div>
-          <div>
-            <button type='submit'>
-              Submit
-            </button>
-          </div>
+                
+
+               
+
+                
+
+                
+               
+            </Grid>
+
+        
+          
         </form>
       </section>
     </>
